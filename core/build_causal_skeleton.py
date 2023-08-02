a747f2120eebc794c429c870ca53865c3dec3266
Daniele Potertì <daniele.poterti@gmail.com>
2023-08-02 10:27:38 +0200

    refactor of build_causal_skeleton.py

--- core/build_causal_skeleton.py
+++ core/build_causal_skeleton.py
@@ -1,17 +1,17 @@
-from causallearn.search.ConstraintBased.PC import pc
-from causallearn.search.ConstraintBased.FCI import fci
-from causallearn.search.ScoreBased.GES import ges
-from causallearn.search.FCMBased import lingam
 from contextlib import contextmanager
 
 import networkx as nx
-import sys
 import os
-import sys, os
+import sys
+
+from causallearn.search.ConstraintBased import FCI, PC
+from causallearn.search.FCMBased import lingam
+from causallearn.search.ScoreBased import GES
 
 
 @contextmanager
 def suppress_stdout():
+    """Suppresses the standard output."""
     with open(os.devnull, "w") as devnull:
         old_stdout = sys.stdout
         sys.stdout = devnull
@@ -20,12 +20,22 @@ def suppress_stdout():
         finally:
             sys.stdout = old_stdout
 
+
 def build_causal_skeleton(df, method='all'):
+    """Builds causal skeleton using given method.
+
+    Args:
+        df: DataFrame to build the skeleton on.
+        method: The method used to build the skeleton, default is 'all'.
+
+    Returns:
+        The result of the skeleton building.
+    """
     methods = {
-        'pc': build_PC,
-        'fci': build_FCI,
-        'ges': build_GES,
-        'lingam': build_LiNGAM,
+        'pc': build_pc,
+        'fci': build_fci,
+        'ges': build_ges,
+        'lingam': build_lingam,
     }
     
     try:
@@ -40,54 +50,93 @@ def build_causal_skeleton(df, method='all'):
     
     return result
 
-def build_PC(df):
+
+def build_pc(df):
+    """Builds PC causal skeleton.
+
+    Args:
+        df: DataFrame to build the PC skeleton on.
+
+    Returns:
+        The result of the PC skeleton building.
+    """
     try:
         data_array = df.values
-        CG = pc(data_array, node_names=df.columns.tolist(), show_progress=False)
-        G = CG.G
+        cg = PC.pc(data_array, node_names=df.columns.tolist(), show_progress=False)
+        g = cg.G
         undirected_graph = nx.Graph()
-        for edge in G.get_graph_edges():
+        for edge in g.get_graph_edges():
             u = edge.get_node1().get_name()
             v = edge.get_node2().get_name()
             undirected_graph.add_edge(u, v)
     except Exception as e:
-        return f"An error occurred in build_PC: {str(e)}"
+        return f"An error occurred in build_pc: {str(e)}"
+    
     return undirected_graph
 
-def build_FCI(df):
+
+def build_fci(df):
+    """Builds FCI causal skeleton.
+
+    Args:
+        df: DataFrame to build the FCI skeleton on.
+
+    Returns:
+        The result of the FCI skeleton building.
+    """
     try:
         data_array = df.values
         with suppress_stdout():
-            G, _ = fci(data_array, verbose = False, show_progress = False)
+            g, _ = FCI.fci(data_array, verbose=False, show_progress=False)
         undirected_graph = nx.Graph()
-        for edge in G.get_graph_edges():
+        for edge in g.get_graph_edges():
             u = edge.get_node1().get_name()
             v = edge.get_node2().get_name()
             undirected_graph.add_edge(u, v)
-        mapping = {"X{}".format(i+1): name for i, name in enumerate(df.columns.tolist())}
+        mapping = {f"X{i+1}": name for i, name in enumerate(df.columns.tolist())}
         undirected_graph = nx.relabel_nodes(undirected_graph, mapping)
     except Exception as e:
-        return f"An error occurred in build_FCI: {str(e)}"
+        return f"An error occurred in build_fci: {str(e)}"
+    
     return undirected_graph
 
-def build_GES(df):
+
+def build_ges(df):
+    """Builds GES causal skeleton.
+
+    Args:
+        df: DataFrame to build the GES skeleton on.
+
+    Returns:
+        The result of the GES skeleton building.
+    """
     try:
         data_array = df.values
         with suppress_stdout():
-            Record = ges(data_array)
-        G = Record['G']
+            record = GES.ges(data_array)
+        g = record['G']
         undirected_graph = nx.Graph()
-        for edge in G.get_graph_edges():
+        for edge in g.get_graph_edges():
             u = edge.get_node1().get_name()
             v = edge.get_node2().get_name()
             undirected_graph.add_edge(u, v)
-        mapping = {"X{}".format(i+1): name for i, name in enumerate(df.columns.tolist())}
+        mapping = {f"X{i+1}": name for i, name in enumerate(df.columns.tolist())}
         undirected_graph = nx.relabel_nodes(undirected_graph, mapping)
     except Exception as e:
-        return f"An error occurred in build_GES: {str(e)}"
+        return f"An error occurred in build_ges: {str(e)}"
+    
     return undirected_graph
 
-def build_LiNGAM(df):
+
+def build_lingam(df):
+    """Builds LiNGAM causal skeleton.
+
+    Args:
+        df: DataFrame to build the LiNGAM skeleton on.
+
+    Returns:
+        The result of the LiNGAM skeleton building.
+    """
     try:
         data_array = df.values
         model = lingam.ICALiNGAM(42, 700)
@@ -96,5 +145,6 @@ def build_LiNGAM(df):
         mapping = {i: name for i, name in enumerate(df.columns.tolist())}
         undirected_graph = nx.relabel_nodes(undirected_graph, mapping)
     except Exception as e:
-        return f"An error occurred in build_LiNGAM: {str(e)}"
+        return f"An error occurred in build_lingam: {str(e)}"
+    
     return undirected_graph
